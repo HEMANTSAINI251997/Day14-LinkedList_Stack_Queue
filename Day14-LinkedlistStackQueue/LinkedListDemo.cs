ea8fc7297045320cf438f9e664e3d8b3c5a33581
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-07-08 13:13:23 +0530

    [HEMANT]Add: Simple Linked List

---
+++ Day14-LinkedlistStackQueue/LinkedListDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_LinkedlistStackQueue
+{
+    public class LinkedListDemo
+    {
+        public void CheckLinkedList()
+        {
+            LinkedList<int> ints = new LinkedList<int>();
+            ints.AddLast(30);
+            ints.AddLast(56);
+            ints.AddFirst(70);
+            foreach(var item in ints)
+            {
+                Console.WriteLine("ELEMEMTS = " + item);
+            }
+        }
+    }
+}
